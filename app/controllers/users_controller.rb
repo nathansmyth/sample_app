2ac36997e057190323164872e3e4667e801fdc73
Nathan Smyth <email@nathansmyth.com>
2012-08-26 15:58:59 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     @title = "Edit page for #{@user.name}"
   end
   def new
+    @user = User.new
     @title = 'Sign up'
+  end
+  def create
+    @user = User.new(params[:user])
+    @title = 'Create user'
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
