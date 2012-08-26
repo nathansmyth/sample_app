2aa0a80bf9de21f25b78aec97d69e5548aec8887
Nathan Smyth <email@nathansmyth.com>
2012-08-25 17:08:45 -0700

    Add Partials; Update Routing; Remove public/index.html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = ''
   end
 
   def help
+    @title = 'Help'
   end
 
   def about
+    @title = 'About Us'
+  end
+
+  def contact
+    @title = 'Contact'
   end
 end
